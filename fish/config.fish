475462178da2240ab56ded9fd70b5c94a65b0fd0
CaptainPickard <pickard.nicko@gmail.com>
2025-10-15 11:20:24 -0600

    fix

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,7 @@ if status is-interactive
 
     # if you made the systemd ssh-agent service, point to its socket:
     set -gx SSH_AUTH_SOCK /run/user/(id -u)/ssh-agent.socket
-
+    ssh-add ~/.ssh/id_ed25519
 
     # Better ls
     alias ls='eza --icons --group-directories-first -1 -lah'
